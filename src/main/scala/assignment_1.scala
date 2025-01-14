e8933ba90bf64aac982bb955f96e53a2d7848476
vaishali <vaishalisahu2913@gmail.com>
2025-01-14 10:57:24 +0000

    Assignment1 answers

---
+++ src/main/scala/assignment_1.scala
@@ -0,0 +1,78 @@
+object assignment_1 {
+  def main(args: Array[String]): Unit = {
+    //  ques1:
+    // Check for Even AND Positive Number:
+    //#Write a Scala function to check if a given number is both even and positive.
+    //#Sample Data:
+    //#Number: 14
+
+      var num = 14
+      if(num%2==0 && num>=0){
+        print("Number is even and positive")
+      }
+      else{
+        print("number is not even")
+      }
+
+    //  2)Range Check with OR:
+    //
+    //    Create a Scala program to determine if a given value is either less than -10 or greater than 10.
+    //
+    //  Sample Data:
+    //
+    //    Value: -15
+
+    //  var num = -15
+    //  if(num < -10 || num>10){
+    //    print("Valid number")
+    //  }
+    //  else{
+    //    print("Invalid number")
+    //  }
+
+    //  3)Odd Number Check with AND:
+    //
+    //    Implement a Scala function to check if a given number is odd and not divisible by 3.
+    //
+    //  Sample Data:
+    //
+    //    Number: 27
+
+    //  var num = 27
+    //  if(num%2 != 0 && num%3 != 0){
+    //    print("valid number")
+    //  }
+    //  else{
+    //    print("invalid")
+    //  }
+
+    //  4)Divisibility by 4 OR 6:
+    //
+    //    Write a Scala program to check if a given number is divisible by either 4 or 6.
+    //
+    //  Sample Data:
+    //
+    //    Number: 18
+
+    //  var num = 18
+    //  if(num % 4 == 0 || num%6 ==0){
+    //    print("Valid number")
+    //  }
+    //  else{
+    //    print("invalid")
+    //  }
+
+    //  5)Eligibility for Voting OR Driving:
+    //
+    //    Create a Scala program to check if a person is eligible to vote (age greater than or equal to 18) or
+    //    eligible to drive (age greater than or equal to 16).
+    //
+    //    Sample Data:
+    //
+    //    Age: 20
+
+
+
+
+  }
+}
