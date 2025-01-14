30c448e8aa01343ff0fa21801cbcee7b620b395b
vaishali <vaishalisahu2913@gmail.com>
2025-01-14 12:37:39 +0000

    Assignment2 answers

---
+++ src/main/scala/Assignment_2.scala
@@ -0,0 +1,26 @@
+object Assignment_2 {
+  def main(args: Array[String]): Unit = {
+
+//1)Print numbers from 1 to 5 using a "for" loop.
+
+    for(i<-1 to 5 by 1){
+      println(i)
+    }
+
+
+
+//    2)Print even numbers from 2 to 10 using a "while" loop.
+//
+//    3)Calculate the sum of all numbers from 1 to 50 using a "for" loop.
+//
+//    4)Print the square of numbers from 1 to 5 using a "for" loop.
+//
+//    5)Print the reverse of a given list using a "while" loop.
+//
+//    6)Generate and print the first 5 multiples of 3 using a "for" loop.
+//
+//    7)Print odd numbers from 1 to 15 using a "while" loop.
+
+
+  }
+}
