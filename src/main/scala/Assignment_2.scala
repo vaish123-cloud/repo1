9bbc202b98a59238da888c7555a493a4a7f0dff2
vaishali <vaishalisahu2913@gmail.com>
2025-01-17 12:18:33 +0000

    Assignment2 answers

--- src/main/scala/Assignment_2.scala
+++ src/main/scala/Assignment_2.scala
@@ -1,26 +1,232 @@
 object Assignment_2 {
   def main(args: Array[String]): Unit = {
 
-//1)Print numbers from 1 to 5 using a "for" loop.
+    //1)Print numbers from 1 to 5 using a "for" loop.
 
-    for(i<-1 to 5 by 1){
-      println(i)
-    }
+    //    for(i<-1 to 5){
+    //      println(i)
+    //    }
+
+//    var a=1
+//    while(a<=5){
+//      println(a)
+//      a=a+1
+//    }
+
+    //    2)Print even numbers from 2 to 10 using a "while" loop.
+
+    //        var a=1
+    //        while(a<=10) {
+    //          if (a % 2 == 0) {
+    //            println(a)
+    //            a = a + 1
+    //            }
+    //           else {
+    //             a = a + 1
+    //           }
+    //        }
+
+//    for(i<-2 to 10 by 2){
+//      println(i)
+//    }
+
+//    var a = 1
+//    while (a <= 10) {
+//      if (a % 2 == 0) {
+//        println(a)
+//      }
+//      a = a + 1
+//    }
 
 
 
-//    2)Print even numbers from 2 to 10 using a "while" loop.
-//
-//    3)Calculate the sum of all numbers from 1 to 50 using a "for" loop.
+    //    3)Calculate the sum of all numbers from 1 to 50 using a "for" loop.
+
+//    var sum=0
+//    for(num<-1 to 50){
+//        sum += num}
+//    print(sum)
+
 //
 //    4)Print the square of numbers from 1 to 5 using a "for" loop.
 //
+//    for(i<-1 to 5){
+//      var num=i*i
+//    println(s"square of number $i is:$num")}
+
+//    for(i<-1 to 5){
+//      println(s"square of number $i is:${i*i}")}
+
 //    5)Print the reverse of a given list using a "while" loop.
+
+//    val l = List(1,2,3,4)
+//    var a=l.length - 1
+//    print(a)
+//    while(a>0){
+//      print(l(a))
+//      a -= 1
+//    }
+
+//    for(i<-l.length-1 to 0 by -1){
+//      println(l(i))
+//    }
+
+
 //
 //    6)Generate and print the first 5 multiples of 3 using a "for" loop.
-//
+
+//    for(i<- 1 to 5){
+//      println(s"first 5 multiple of 3 is ${3*i}")
+//    }
+
+//        for(i<- 1 to 5){
+//        var  a=3*i
+//          println(a)
+//        }
+////
 //    7)Print odd numbers from 1 to 15 using a "while" loop.
+//     var a=1
+//    while(a<=15){
+//      if(a%2!=0){
+//        println(a)
+//      }
+//      a=a+1
+//    }
+
+//    var a=1
+//    while(a<=15){
+//      println(a)
+//      a=a+2
+//    }
+
+//    8)Calculate the factorial of a given number using a "for" loop.
+
+//    var number = 5
+//    var a =1
+//    for(i<-1 to number){
+//      a *= i
+//    }
+//    println(a)
+//
+//    9)Print the characters of a string in reverse order using a "for" loop.
+
+//    var a="hello"
+
+//    for(i <-a.length-1 to 0 by -1){
+//      print(a(i))
+//    }
+
+//
+//    10)Check if a number is prime using a "while" loop.
+//
 
 
-  }
-}
+//    11)Print the elements of a list in reverse order using a "for" loop.
+
+//    var l = List(1,2,3,4)
+//    for(i <- l.length-1 to 0 by -1){
+//      println(l(i))
+//    }
+//
+//    12)Find the sum of all even numbers from 1 to 20 using a "while" loop.
+
+//    var sum=0
+//    var a=1
+//    while(a<=20){
+//      if(a%2==0){
+//        sum +=a
+//      }
+//      a=a+1
+//    }
+//    print(sum)
+//
+//    Print the pattern:
+
+//    for(i <-1 to 4){
+//      for(j<-1 to i){
+//        print("*")
+//      }
+//      println(" ")
+//    }
+//    13)Check if a string is a palindrome using a "for" loop.
+
+//    var a="ollo"
+//    var index=a.length-1
+//
+//
+//    for(i<-index to  0 by -1){
+//      print(a(i))
+//    }
+
+//
+//    14)Print the cube of numbers from 1 to 3 using a "while" loop.
+
+//    var a=1
+//    while(a<=3){
+//      println(s"cube of number $a is ${a*a*a}")
+//      a=a+1
+//    }
+
+//
+//    15)Count the number of vowels in a given string using a "for" loop.
+//    var count=0
+//    var mystrings= "aeroplane"
+//    var vowel="aeiouAEIOU"
+//    for(i <- mystrings){
+//      if(i == 'a'|| i == 'e'){
+//        count +=1
+//      }
+//  print(count)
+//    }
+//
+//    16)Print the elements of a list using a "while" loop.
+
+
+//    var l = List(1,2,3,4,5)
+//    var index = 0
+//    while(index <=l.length){
+//      println(l(index))
+//      index=index+1
+//
+//    }
+
+//
+//    17)Calculate the product of numbers from 1 to 5 using a "for" loop.
+
+//var product=1
+//  for(i<- 1 to 5){
+//    product*=i
+//
+//  }
+//    print(product)
+
+
+//
+//
+//    Print the pattern:
+
+//    for(i<-1 to 4){
+//      for(j<-1 to i){
+//        print(i)
+//      }
+//      println(" ")
+//    }
+
+
+
+
+//    18)Check if a given number is a perfect square using a "while" loop.
+
+//var number=25
+//    var i=1
+//    while(i*i < number){
+//      i += 1
+//    }
+//if(i*i == number){
+//  print(s"$number is perfect square")
+//}
+//else{
+//  print(s"$number is not perfect sqaure")
+//}
+//  }
+//}
