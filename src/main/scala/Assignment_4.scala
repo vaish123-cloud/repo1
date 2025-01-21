f21fb528773263b47b909030cfdde6cc2655172c
vaishali <vaishalisahu2913@gmail.com>
2025-01-21 08:23:01 +0000

    Assignment4 answers

---
+++ src/main/scala/Assignment_4.scala
@@ -0,0 +1,129 @@
+object Assignment_4 {
+  def main(args: Array[String]): Unit = {
+    //    Pattern Related Programs
+    //
+    //    1)Program to print a right-angled triangle pattern
+    //
+    //    **
+    //
+    //    *
+    //
+    //    *
+
+    //    for(i<-1 to 5){
+    //      for(j<- 1 to i ){
+    //        print("*")
+    //      }
+    //      println(" ")
+    //    }
+
+    //
+    //    2)Program to print a square pattern
+    //
+    //    **
+    //    **
+
+    //        for(i<-1 to 4){
+    //          for(j<- 1 to 4 ){
+    //            print("*")
+    //          }
+    //          println(" ")
+    //        }
+
+    //    3)Inverted Triangle Pattern:
+
+    //    for(i<-5 to 1 by -1){
+    //      for(j<- 1 to i by 1 ){
+    //        print("*")
+    //      }
+    //      println(" ")
+    //    }
+
+    //    4)HOLLOW PATTERN
+
+    //        for(i<-1 to 4 ){
+    //          for(j<-1 to 6){
+    //            if(i==1 || i==4||j==1||j==5) {
+    //              print("*")
+    //            }
+    //            else{
+    //              print(" ")
+    //            }
+    //          }
+    //          println(" ")
+    //        }
+
+
+    //    5)
+    //
+    //    *
+    //
+    //    **
+    //
+    //    ****
+    //
+    //    *****
+
+
+    //    for(i<-1 to 5){
+    //      for(j<-1 to i){
+    //        if(i==1 || j==1) {
+    //          print("*")
+    //        }
+    //        else{
+    //          print("_*")
+    //        }
+    //      }
+    //      println(" ")
+    //    }
+
+    //6.
+    //        // First part of the pattern
+    //        for (i <- 1 to 9 by 2) {
+    //          println("*" * i )
+    //        }
+    //
+    //        // Second part of the pattern
+    //        for (i <- 7 to 1 by -2) {
+    //          println("*" * i)
+    //        }
+    //      }
+
+    //7.
+
+//    for (i <- 7 to 1 by -2) {
+//      println("*" * i)
+//    }
+//    for (j <- 1 to 7 by 2) {
+//      println("*" * j)
+//    }
+
+    //8. butterfly pattern
+
+//    for(i<-1 to 5 ){
+//      if(i==1|i==5) {
+//        println("*   *")
+//      }
+//      else if(i==2||i==4){
+//        println("** **")
+//      }
+//      else if(i==3) {
+//        println("**"*i)
+//      }
+//    }
+
+    //9.
+
+//    for(i<-1 to 5){
+//      println("*"*i)
+//    }
+//    for(i<-4 to 1 by -1){
+//      println("*"*i)
+//    }
+
+    //10.
+
+
+    }
+  }
+}
